0d1b1c26e7f1880425261c16ce78cdb189094146
Pedro Lage <lagecomg@gmail.com>
2025-08-12 23:06:45 -0300

    Leitura inicial e inspeção dos dados

--- analise.py
+++ analise.py
@@ -1,1 +1,12 @@
-print("Iniciando minha análise de dados de e-commerce...")
+import pandas as pd
+
+df = pd.read_csv("data.csv", encoding="ISO-8859-1")
+
+print("Primeiras linhas do dataset:")
+print(df.head())
+
+print("\nInformações do dataset:")
+print(df.info())
+
+print("\nEstatísticas descritivas:")
+print(df.describe())
